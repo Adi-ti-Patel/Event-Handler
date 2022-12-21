f53a6519ae329e262902da8629c046ad738e2da0
Aditi <110724956+Adi-ti-Patel@users.noreply.github.com>
2022-12-21 13:16:36 +0530

    Add files via upload

---
+++ EventHandler_ICICI/EventHandler_ICICI/EventHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace EventHandler_ICICI
+{
+    public class CreditCard
+    {
+        private string creditCardNo;
+        private string cardHolderName;
+        private double balanceAmount;
+        private double creditLimit;
+
+        //public delegate void SendSMS(string message);
+
+        public event Action<string> Transaction;
+
+        public CreditCard(string creditCardNo, string cardHolderName, double balanceAmount, double creditLimit)
+        {
+            this.creditCardNo = creditCardNo;
+            this.cardHolderName = cardHolderName;
+            this.balanceAmount = balanceAmount;
+            this.creditLimit = creditLimit;
+        }
+
+        public double GetBalance()
+        {
+            return balanceAmount;
+        }
+
+        public double GetCreditLimit()
+        {
+            return creditLimit;
+        }
+
+        public void MakePayment(double amount)
+        {
+            if (amount <= (creditLimit - balanceAmount))
+            {
+                this.balanceAmount += amount;
+                if (Transaction != null)
+                {
+                    Transaction($"{this.cardHolderName} has made payment for amount {amount}. The balance is {this.balanceAmount} and available credit is {this.creditLimit - this.balanceAmount}");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ EventHandler_ICICI/EventHandler_ICICI/Program.cs
@@ -0,0 +1,13 @@
+﻿using EventHandler_ICICI;
+
+CreditCard creditCard = new CreditCard("465465", "Aditi", 0, 50000);
+creditCard.Transaction += SendSMS;
+creditCard.MakePayment(100);
+creditCard.MakePayment(1000);
+
+Console.ReadLine();
+
+static void SendSMS(string message)
+{
+    Console.WriteLine(message);
+}
